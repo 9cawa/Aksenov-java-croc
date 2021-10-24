8f04e96c874edc7945af28863870de52d0ed60b5
Aleksandr <sasha.aksenov.01@mail.ru>
2021-10-24 19:57:10 +0300

    Задачи первой недели сданы

--- week1/src/ru/croc/task3/Main.java
+++ week1/src/ru/croc/task3/Main.java
@@ -11,13 +11,8 @@ public class Main {
         //Делаем из строки массив интов
         int[] arrInt = setArray(arr);
 
-        //Поиск индексов максимального и минимального элементов
-        int minIndex = findMinAndMax(arrInt)[0];
-        int maxIndex = findMinAndMax(arrInt)[1];
-
-        //Меняем местами максимальный и минимальный элементы с элементами, стоящими в начале и в конце
-        arrInt = swap(arrInt, minIndex, maxIndex);
-
+        //Сортируем массив по возрастанию
+        Arrays.sort(arrInt);
         System.out.println("Результат: " + Arrays.toString(arrInt));
     }
 
@@ -28,41 +23,5 @@ public class Main {
             arrInt[i] = Integer.parseInt(arrStr[i]);
         }
         return arrInt;
-    }
-
-    static int[] findMinAndMax(int[] arrInt) {
-        int minIndex = 0;
-        int maxIndex = 0;
-        for (int i = 0; i < arrInt.length; i++) {
-            if (arrInt[i] > arrInt[maxIndex]) {
-                maxIndex = i;
-            }
-            if (arrInt[i] < arrInt[minIndex]) {
-                minIndex = i;
-            }
-        }
-        return new int[] {minIndex, maxIndex};
-    }
-
-    static int[] swap(int[] arrInt, int minIndex, int maxIndex) {
-        int max = arrInt[maxIndex];
-        int min = arrInt[minIndex];
-        if (maxIndex != 0 && minIndex != 1) {
-            arrInt[minIndex] = arrInt[0];
-            arrInt[maxIndex] = arrInt[arrInt.length - 1];
-            arrInt[0] = min;
-            arrInt[arrInt.length - 1] = max;
-            return arrInt;
-        //Проверка на тонкий момент граничного случая и его обработка
-        } else {
-            arrInt[minIndex] = arrInt[0];
-            arrInt[0] = min;
-
-            int tmp = arrInt[arrInt.length - 1];
-            arrInt[arrInt.length - 1] = max;
-            arrInt[minIndex] = tmp;
-
-            return arrInt;
-        }
     }
 }
